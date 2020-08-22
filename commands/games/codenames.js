349d66c2a8ec79ae9c9dae2b6ffe227f346be729
pigsnsheep <47608502+pigsnsheep@users.noreply.github.com>
2020-08-22 16:38:20 -0700

    Update codenames.js

--- commands/games/codenames.js
+++ commands/games/codenames.js
@@ -1,1 +1,31 @@
-words = ["doctor","eagle","oil","plastic","elephant","Germany","nail","hammer","electricity","milk","pipe","computer","Discord","Lifeline","Steam"]
+var words = ["doctor","eagle","oil","plastic","elephant","Germany","nail","hammer","electricity","milk","pipe","computer","Discord","Lifeline","Steam","Orange"];
+var assasinWord = "";
+var blueWords = [];
+var redWords = [];
+var tempIndex;
+var element;
+
+for (i = 0; i < 5; i++){
+    tempIndex = Math.floor(Math.random()*words.length);
+    element = words[tempIndex]
+    blueWords.push(element);
+    words = words.filter(el => el != element) 
+}
+
+for (i = 0; i < 5; i++){
+    tempIndex = Math.floor(Math.random()*words.length);
+    element = words[tempIndex]
+    redWords.push(element);
+    words = words.filter(el => el != element) 
+}
+
+tempIndex = Math.floor(Math.random()*words.length);
+assasinWord = words[tempIndex] 
+
+
+console.log(assasinWord)
+console.log(blueWords)
+console.log(redWords)
+console.log(words.length)
+
+
