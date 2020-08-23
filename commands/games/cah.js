969843898a7df5be24af418dc364548234acf002
pitabread8 <70041182+pitabread8@users.noreply.github.com>
2020-08-22 17:29:28 -0700

    Update cah.js

--- commands/games/cah.js
+++ commands/games/cah.js
@@ -13,49 +13,50 @@ module.exports = class CAHCommand extends Command {
 	}
 
 	run(message) {
-        let blackcards = [];
-        
-        var i = 0;
-        for (i; i < stuff.blackCards.length; i++) {
-            blackcards.push(stuff.blackCards[i].text)
-        }
+		let blackcards = [];
 
-        let prompt = blackcards[Math.floor(Math.random() * blackcards.length)];
-        message.say(`Here is your black card: "${prompt}"`);
+		var i = 0;
+		for (i; i < stuff.blackCards.length; i++) {
+		    blackcards.push(stuff.blackCards[i].text)
+		}
 
-        let whitecards = [];
-        for (i = 0; i < stuff.whiteCards.length; i++) {
-            whitecards.push(stuff.whiteCards[i])
-        }
+		let prompt = blackcards[Math.floor(Math.random() * blackcards.length)];
+		message.say(`Here is your black card: "${prompt}"`);
 
-        let options = [];
+		let whitecards = [];
+		for (i = 0; i < stuff.whiteCards.length; i++) {
+		    whitecards.push(stuff.whiteCards[i])
+		}
 
-        for (i = 0; i < 10; i++) {
-            let randint = Math.floor(Math.random() * whitecards.length)
-            options.push(whitecards[randint]);
-        }
-        
-        let dict = {};
+		let options = [];
 
-        for (i = 0; i < options.length; i++) {
-            dict[options[i]] = Math.floor(Math.random() * 100)
-        }
+		for (i = 0; i < 10; i++) {
+		    let randint = Math.floor(Math.random() * whitecards.length)
+		    options.push(whitecards[randint]);
+		}
 
-        const whitecardsEmbed = new Discord.MessageEmbed()
-            .setColor('#fffffe')
-            .setAuthor(`${message.author.username}'s CAH cards`, message.author.displayAvatarURL())
-            .setDescription(`Here are your CAH cards. If you would like to play any of them, enter the number *below* the white card in #${message.channel.name}.`)
+		let dict = {};
 
-        for(var key in dict) {
-            whitecardsEmbed.addField(`"${key}"`, dict[key])
-        }
-        message.author.send(whitecardsEmbed)
-      
-        const filter = m => m.author.id === message.author.id;
-        const collector = message.channel.createMessageCollector(filter, { max: 1 });
+		for (i = 0; i < options.length; i++) {
+		    dict[options[i]] = Math.floor(Math.random() * 100)
+		}
 
-        collector.on('collect', m => {
-            console.log(m.content)
-        });
+		const whitecardsEmbed = new Discord.MessageEmbed()
+		    .setColor('#fffffe')
+		    .setAuthor(`${message.author.username}'s CAH cards`, message.author.displayAvatarURL())
+		    .setDescription(`Here are your CAH cards. If you would like to play any of them, enter the number *below* the white card in #${message.channel.name}.`)
+
+		for(var key in dict) {
+		    whitecardsEmbed.addField(`"${key}"`, dict[key])
+		}
+		message.author.send(whitecardsEmbed)
+		
+		// Not useful yet.
+		const filter = m => m.author.id === message.author.id;
+		const collector = message.channel.createMessageCollector(filter, { max: 1 });
+
+		collector.on('collect', m => {
+		    console.log(m.content)
+		});
 	}
 };
